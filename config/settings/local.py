24f8bc4846adf4324adc6c9e971c08b2beadc0ea
Michal Urban <mcraftpl2@gmail.com>
2023-02-28 12:40:30 +0100

    updated

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,9 +1,6 @@
 # from config.settings.base import BASE_DIR
 from .base import *
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
